d6f4e0fb32810a62eb9e38bc8a2c571149e634b5
anonymous0035 <alexsunnyone@gmail.com>
2025-06-17 10:56:29 +0500

    Update LeadsManagement.tsx

--- frontend/components/admin/LeadsManagement.tsx
+++ frontend/components/admin/LeadsManagement.tsx
@@ -21,7 +21,7 @@ export default function LeadsManagement() {
   const fetchLeads = async () => {
     setIsLoading(true)
     try {
-      const response = await fetch(`${backend_url}api/get-all-leads`)
+      const response = await fetch(`${backend_url}/api/get-all-leads`)
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`)
       }
